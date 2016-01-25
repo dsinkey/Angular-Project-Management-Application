4d0bc226e7626d6451f000a107ecfb7a907ae1af
Dustin <dustinsinkey1@gmail.com>
2016-01-24 20:06:47 -0800

    Edited controller.js to use routes to set tabs

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,4 +1,15 @@
 'use strict';
-angular.module('myApp.controllers', []).controller('NgAnimateCtrl', function ($scope) {
+angular.module('myApp.controllers', []).controller('ViewIssueCtrl', function ($scope, $route, $element, $timeout) {
+  $scope.$watch(function(){ return $route.current && &route.current.params.issue;}, function(newVal, oldVal){
+    $scope.showTab = !!newVal;
+    $scope.issueId = newVal;
 
+    $timeout(function() {
+      if($scope.showTab) {
+        $element.find('a').click();
+      } else {
+        $element.siblings().first().find('a').click();
+      }
+    }, 0, false);
+  });
 });
